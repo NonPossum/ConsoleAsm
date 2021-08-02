8e7ee8bad3d28751dfce51f0bfd97b657f41ac81
DeVianney <jacquesvianney3931@gmail.com>
2021-08-02 18:48:35 +0200

    Add files via upload

---
+++ ConsoleAsm/asmstack.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace ConsoleAsm
+{
+    class asmstack
+    {
+        public static int stacksize = 0x1024;
+
+        private static int Top = 0;
+        public static int[] stack = new int[stacksize];
+        public static (int Start, int End) stackframe = (0, 0);
+        public void Pop()
+        {
+            int top = 0;
+            if (stackframe.End == 0)
+            {
+                top = Top;
+            }
+            else
+            {
+                top = Top + stackframe.End;
+            }
+            if (top > 0)
+            {
+                stack[top--] = 0;
+            }
+            else if (top == 0)
+            {
+                stack[top] = 0;
+            }
+        }
+        public void Push(int x)
+        {
+            int top = 0;
+            if (stackframe.End == 0)
+            {
+                top = Top;
+            }
+            else
+            {
+                top = Top + stackframe.End;
+            }
+            if (top == stacksize)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Stack Overflow");
+                Console.ResetColor();
+            }
+            else if (top == 0)
+            {
+                stack[top++] = x;
+            }
+            else
+            {
+                stack[top] = x;
+            }
+
+        }
+
+        public void InitialStackFrame()
+        {
+            // mov ebp, esp
+            stackframe.Start = Top;
+
+        }
+
+        public void DestroyStackFrame()
+        {
+            // ret
+            Array.Clear(stack, stackframe.Start, stackframe.Start - stackframe.End);
+            stackframe = (0, 0);
+        }
+        public static void SetStackFrameValue(int ebp, int value)
+        {
+            if (ebp == 0) { ebp = 1 + stackframe.Start; }
+            else { ebp = ebp + stackframe.Start; };
+            stack[ebp] = value;
+            stackframe.End++;
+
+        }
+        public void GetValueWithStackFrame()
+        {
+
+        }
+        public void StackSize()
+        {
+
+            int i = 0;
+            for (; i < stacksize; i++)
+            {
+                //If value is equals 0 ??
+                if (stack[i] == 0)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(i);
+        }
+
+    }
+
+}
---
+++ ConsoleAsm/registers.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+
+namespace ConsoleAsm
+{
+    class registers
+    {
+
+        public static byte bits = 64;
+
+        public static BitArray flagReg = new BitArray(32);
+        // cf pf af zf sf of nt rf vip id...
+
+        public static BitArray rax = new BitArray(bits);
+        /*
+        Values are returned from functions in this register. 
+        scratch rax	eax	ax	ah and al
+        */
+
+        public static BitArray rcx = new BitArray(bits);
+        /*
+        Typical scratch register.  Some instructions also use it as a counter.	scratch
+        rcx	ecx	cx	ch and cl
+        */
+        public static BitArray rdx = new BitArray(bits);
+        /*
+        Scratch register.	scratch
+        rdx	edx	dx	dh and dl*/
+
+        public static BitArray rbx = new BitArray(bits);
+        /*
+        Preserved register: don't use it without saving it!	preserved
+        rbx	ebx	bx	bh and bl*/
+        public static BitArray rsp = new BitArray(bits);
+        /*
+        The stack pointer.  Points to the top of the stack (details coming soon!)
+        preserved	rsp	esp	sp	spl*/
+        public static BitArray rbp = new BitArray(bits);
+        /*
+        Preserved register.  Sometimes used to store the old value of the stack pointer, or the "base".	preserved
+        rbp	ebp	bp	bpl
+        */
+        public static BitArray rsi = new BitArray(bits);
+        /*
+        Scratch register. 
+        rsi	esi	si	sil
+        */
+        public static BitArray rdi = new BitArray(bits);
+        /*
+        Scratch register.
+        rdi	edi	di	dil
+        */
+
+        public void SetValue(BitArray reg, int bits)
+        {
+
+        }
+
+
+    }
+
+
+}
