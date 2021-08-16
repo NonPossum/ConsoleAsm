e64482c431eb754c6bc358ffdb67d250d96553e4
DeVianney <jacquesvianney3931@gmail.com>
2021-08-16 19:00:37 +0300

    Add files via upload

---
+++ ConsoleAsm/callfunctions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAsm
+{
+    class callfunctions
+    {
+
+        public static void malloc()
+        {
+
+        }
+        public static void free()
+        {
+
+        }
+        public static void printf()
+        {
+
+        }
+
+    }
+}
